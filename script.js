1202fb66ea625a1e6aed2274bf1a00db3153e822
Matviy987 <matvii2013marshalek@gmail.com>
2026-02-07 13:54:07 +0200

    сробив завдання

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', () =>{
-    const input = document.querySelector("#input");
-    const Btn = document.querySelector("#translateBtn")
 
+    const input = document.querySelector("#input");
+    const Btn = document.querySelector("#translateBtn");
+    const output = document.querySelector('#output');
+
+    async function translateText(){
+        const text = input.value.trim()
+        const safeText = encodeURIComponent(text)
+        const email = "matvii2013marshalek@gmail.com"
+        const url = `https://api.mymemory.translated.net/get?q=${safeText}&langpair=uk|en&de=${email}`;
+
+        const response = await fetch(url)
+        const data = await response.json()
+
+        console.log(data)
+        output.textContent = data.responseData.translatedText
+    }
+
+        Btn.addEventListener('click', translateText);
+    
 });
